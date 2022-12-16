c0e8846497174adf8f920dd17c10370fdc6cd4ff
Luiz Torre <69131416+Luiz-Torre@users.noreply.github.com>
2022-12-15 22:48:21 -0300

    Update home.dart

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -5,7 +5,7 @@ import 'cadastro.dart';
 import 'login.dart';
 
 class Home extends StatelessWidget {
-    static int? var_logado;
+  static int? var_logado;
 
   const Home({super.key});
   @override
@@ -15,14 +15,15 @@ class Home extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blueGrey,
       ),
-      home: const MyHomePage(title: 'Loja de Anúncios'),
+      home: MyHomePage(title: 'Loja de Anúncios'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
+  String? email_logado;
 
-  const MyHomePage({super.key, required this.title});
+  MyHomePage({super.key, required this.title});
 
   final String title;
 
@@ -30,103 +31,209 @@ class MyHomePage extends StatefulWidget {
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
-Future<int> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  SharedPreferences prefs = await SharedPreferences.getInstance();
-  var email = prefs.getString("email");
-  if (email != null && email != '') {
-    return 1;
-  } else {
-    return 0;
-  }
-}
-
 class _MyHomePageState extends State<MyHomePage> {
+  String? email_logado;
+
+  Future<int> main() async {
+    WidgetsFlutterBinding.ensureInitialized();
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    var email = prefs.getString("email");
+
+    if (email != null && email != '') {
+      return 1;
+    } else {
+      return 0;
+    }
+  }
+
+  signOut() async {
+    SharedPreferences preferences = await SharedPreferences.getInstance();
+
+    setState(() {
+      preferences.setInt("value", 0);
+      preferences.setString("email", '');
+
+      LoginPage.loginStatus = LoginStatus.notSignIn;
+    });
+    LoginPage.loginStatus = LoginStatus.notSignIn;
+    Home.var_logado = 0;
+    Navigator.push(
+        context, MaterialPageRoute(builder: (context) => const Home()));
+  }
+
   @override
   Widget build(BuildContext context) {
     main().then((retorno) {
       Home.var_logado = retorno;
+      setState(() {});
+
     });
-    return Scaffold(
-        appBar: AppBar(
-          title: Text(
-            widget.title,
-            style: TextStyle(color: Colors.white),
+    if (Home.var_logado != 0) {
+      return Scaffold(
+          appBar: AppBar(
+            title: Text(
+              widget.title,
+              style: TextStyle(color: Colors.white),
+            ),
           ),
-        ),
-        body: Column(children: [
-          Center(
-            child: Column(
-              children: <Widget>[
-                OutlinedButton(
-                  onPressed: () {
-                    if (Home.var_logado == 0) {
+          body: Column(children: [
+            Center(
+              child: Column(
+                children: <Widget>[
+                  OutlinedButton(
+                    onPressed: () {
+                      signOut();
+                    },
+                    child: Container(
+                      padding: EdgeInsets.all(20.0),
+                      child: Column(
+                        children: <Widget>[
+                          Text('Logout'),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Center(
+              child: Column(
+                children: <Widget>[
+                  OutlinedButton(
+                    onPressed: () {
                       Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (context) => const LoginPage()));
-                    } else {
-                      ScaffoldMessenger.of(context).showSnackBar(
-                        const SnackBar(content: Text('Já está logado')),
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const Cadastro()),
                       );
-                    }
-                  },
-                  child: Container(
-                    padding: EdgeInsets.all(20.0),
-                    child: Column(
-                      children: <Widget>[
-                        Text('Fazer Login'),
-                      ],
+                    },
+                    child: Container(
+                      padding: EdgeInsets.all(20.0),
+                      child: Column(
+                        children: <Widget>[
+                          Text('Cadastrar Conta'),
+                        ],
+                      ),
                     ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
-          ),
-          Center(
-            child: Column(
-              children: <Widget>[
-                OutlinedButton(
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => const Cadastro()),
-                    );
-                  },
-                  child: Container(
-                    padding: EdgeInsets.all(20.0),
-                    child: Column(
-                      children: <Widget>[
-                        Text('Cadastrar Conta'),
-                      ],
+            Center(
+              child: Column(
+                children: <Widget>[
+                  OutlinedButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const Produto()),
+                      );
+                    },
+                    child: Container(
+                      padding: EdgeInsets.all(20.0),
+                      child: Column(
+                        children: <Widget>[
+                          Text('Ver produtos'),
+                        ],
+                      ),
                     ),
                   ),
-                ),
-              ],
+                ],
+              ),
+            ),
+          ]));
+    } else {
+      setState(() {});
+
+      return Scaffold(
+        
+          appBar: AppBar(
+            title: Text(
+              widget.title,
+              style: TextStyle(color: Colors.white),
             ),
           ),
-          Center(
-            child: Column(
-              children: <Widget>[
-                OutlinedButton(
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => const Produto()),
-                    );
-                  },
-                  child: Container(
-                    padding: EdgeInsets.all(20.0),
-                    child: Column(
-                      children: <Widget>[
-                        Text('Ver produtos'),
-                      ],
+          body: Column(children: [
+            Center(
+              child: Column(
+                children: <Widget>[
+                  OutlinedButton(
+                    onPressed: () {
+                      if (Home.var_logado == 0) {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => const LoginPage()));
+                      } else {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          const SnackBar(content: Text('Já está logado')),
+                        );
+                      }
+                    },
+                    child: Container(
+                      padding: EdgeInsets.all(20.0),
+                      child: Column(
+                        children: <Widget>[
+                          Text('Fazer Login'),
+                        ],
+                      ),
                     ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
-          ),
-        ]));
+            Center(
+              child: Column(
+                children: <Widget>[
+                  OutlinedButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const Cadastro()),
+                      );
+                    },
+                    child: Container(
+                      padding: EdgeInsets.all(20.0),
+                      child: Column(
+                        children: <Widget>[
+                          Text('Cadastrar Conta'),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Center(
+              child: Column(
+                children: <Widget>[
+                  OutlinedButton(
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const Produto()),
+                      );
+                    },
+                    child: Container(
+                      padding: EdgeInsets.all(20.0),
+                      child: Column(
+                        children: <Widget>[
+                          Text('Ver produtos'),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            
+          ])
+          
+          );
+
+    }
   }
 }
